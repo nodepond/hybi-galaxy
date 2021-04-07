8a7d52de0ec7bb0082c4651dc7eb96da8ae200ee
Martin Wisniowski <mw@node3000.com>
2021-04-08 00:19:47 +0200

    Load foyer as background image

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { panOptions } from '../PanWrapper/panOptions';
+import backgroundImage from '../../assets/hybifoyer.jpg'
 
 /* fixed size won't work, because when scale is 1 there will be room to pan; but the plugin won't allow it because scale is 1. 
 the fix is to set the size of the react-transform-component and react-transform-element exlusively (see App.css) */
@@ -13,7 +14,7 @@ const RoomContainer = styled.div`
 `
 
 const Background = styled.div`
-  background-image:url("/build/favicon.ico");
+  background-image:url(${backgroundImage});
   opacity:0.1;
   width:100%;
   height:100%;
@@ -22,8 +23,8 @@ const Background = styled.div`
 export const Room:React.FC = ({children}) => {
   return (
     <RoomContainer>
-      {/* <Background /> */}
       {children}
+      <Background />
     </RoomContainer>
   )
 }
