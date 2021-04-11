fc3ba0befb84f921da6011fc3febef4e63a422a9
Martin Wisniowski <mw@node3000.com>
2021-04-11 22:34:17 +0200

    Make own avatar movable again

--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import { panOptions } from '../PanWrapper/panOptions';
-import backgroundImage from '../../assets/hybifoyer.jpg'
+import backgroundImage from '../../assets/hybifoyer.jpg';
 
 /* fixed size won't work, because when scale is 1 there will be room to pan; but the plugin won't allow it because scale is 1. 
 the fix is to set the size of the react-transform-component and react-transform-element exlusively (see App.css) */
-
 
 const RoomContainer = styled.div`
   width:${panOptions.room.size.x}px;height:${panOptions.room.size.y}px;
@@ -15,9 +14,10 @@ const RoomContainer = styled.div`
 
 const Background = styled.div`
   background-image:url(${backgroundImage});
-  opacity:0.1;
+  opacity:0.3;
   width:100%;
   height:100%;
+  pointer-events: none;
 `
 
 export const Room:React.FC = ({children}) => {
