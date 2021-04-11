a1d67019cab46f262e3e761d327034f601ab32a7
Martin Wisniowski <mw@node3000.com>
2021-04-11 14:59:34 +0200

    Linting

--- src/components/PanWrapper/panOptions.ts
+++ src/components/PanWrapper/panOptions.ts
@@ -4,7 +4,7 @@ export const panOptions = {
   room: { size: { x: 6000, y: 6000 } },
     get user() {
       return {
-        //center the room
+        // center the room
         initialPosition: { x: this.room.size.x / 2 - (Math.random() * 200 - 100), y: this.room.size.y / 2 - (Math.random() * 200 - 100) },
         size: { x: 200, y: 200 },
       }
@@ -14,7 +14,7 @@ export const panOptions = {
 export const transformWrapperOptions: PropsList = {
   wheel: { step: 50 },
   scale: 1.0,
-  //center the window, considering the size of the user view
+  // center the window, considering the size of the user view
   defaultPositionX: -panOptions.user.initialPosition.x +(window.innerWidth-panOptions.user.size.x)/2,
   defaultPositionY: -panOptions.user.initialPosition.y+(window.innerHeight-panOptions.user.size.y)/2,
   positionX: 0,
