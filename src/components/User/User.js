136676326ced90572d12b27a31334653fbc4f8fa
Martin Wisniowski <mw@node3000.com>
2021-04-21 18:14:36 +0200

    Indicate room at user

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -10,6 +10,7 @@ export const User = ({id, user}) => {
 
   const myPos = useConferenceStore(useCallback(store => store.users[id]['pos'], [id]))
   const myVolume = useConferenceStore(useCallback(store => store.users[id]['volume'], [id]))
+  const myRoom = useConferenceStore(useCallback(store => store.users[id]['room'], [id]))
   const isMute = useConferenceStore(useCallback(store => store.users[id]['mute'],[id]))
   const calculateVolume = useConferenceStore(useCallback(store => store.calculateVolume, []))
   useEffect(() => {
@@ -23,6 +24,7 @@ export const User = ({id, user}) => {
       <AudioTrack id={id} volume={myVolume} />
       <NameTag>{user?.user?._displayName || 'Friendly Sphere'}</NameTag>
       <div>Volume {Math.round(myVolume * 11)}</div>
+      <div>Room {myRoom}</div>
       {isMute && <MuteIndicator>🤭</MuteIndicator>}
     </div>
   )
