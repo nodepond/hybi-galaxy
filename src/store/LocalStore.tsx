9f00c958b9dd9759588578e06d354512200806ef
Martin Wisniowski <mw@node3000.com>
2021-05-19 16:02:42 +0200

    Implement room and seat collide code

--- src/store/LocalStore.tsx
+++ src/store/LocalStore.tsx
@@ -4,7 +4,6 @@ import { Track, User } from "./ConferenceStore";
 import { panOptions, transformWrapperOptions } from "../components/PanWrapper/panOptions";
 import { mountStoreDevtool } from "simple-zustand-devtools";
 
-
 export type Point = {x:number, y:number}
 type ZoomPan = {
   pos:Point
@@ -37,20 +36,14 @@ export const useLocalStore = create<Store>((set,get) => {
   // # Private Functions
   const _produceAndSet = (callback:(newState:Store)=>void)=>set(state => produce(state, newState => callback(newState)))
 
-  
-  // # Public Functions
-  const setLocalPosition = (newPosition) => {
-    set({pos:newPosition})
+  const isHittingRoom = (newPosition, roomname = "room1") => {
     let rootSvg = document.getElementById('RootSvg')
-    let zxc = document.getElementsByClassName('room2')
-    console.log('zxc-d')
-    console.log('zxc', zxc)
+    let zxc = document.getElementsByClassName(roomname)
     // const zxcIterable = Array.from(zxc)
     const isHitting = Array.prototype.map.call(zxc, path => {
-
       // please notice, that right at the moment isPointInFill() with DOMPoints only works in safari right now. This is why we use the version that inits with SVGPoint. Works on most modern browsers atm.
       // return path.isPointInFill(new DOMPoint(newPosition.x, newPosition.y))
-      
+
       // @ts-ignore
       let myPoint = rootSvg.createSVGPoint()
       myPoint.x = newPosition.x
@@ -59,16 +52,98 @@ export const useLocalStore = create<Store>((set,get) => {
     })
     // const isHitting = zxcIterable.map()
     console.log('isHitting', isHitting)
+    return isHitting.includes(true)
+  }
 
-    if (newPosition.x < 2500-100) {
+  // # Public Functions
+  const setLocalPosition = (newPosition) => {
+    set({pos:newPosition})
+    
+    if (isHittingRoom(newPosition, "room1")) {
       set({room: 'room-1'})
-    } else if (
-      newPosition.x < 3500-100 && 
-      newPosition.x > 2500-100 &&
-      newPosition.y < 3100-100 &&
-      newPosition.y > 1700-100) {
+    }
+    else if (isHittingRoom(newPosition, "room2")) {
       set({room: 'room-2'})
-    } else {
+    }
+    else if (isHittingRoom(newPosition, "room3")) {
+      set({room: 'room-3'})
+    }
+    else if (isHittingRoom(newPosition, "seat1")) {
+      set({room: 'seat-1'})
+    }
+    else if (isHittingRoom(newPosition, "seat2")) {
+      set({room: 'seat-2'})
+    }
+    else if (isHittingRoom(newPosition, "seat3")) {
+      set({room: 'seat-3'})
+    }
+    else if (isHittingRoom(newPosition, "seat4")) {
+      set({room: 'seat-4'})
+    }
+    else if (isHittingRoom(newPosition, "seat5")) {
+      set({room: 'seat-5'})
+    }
+    else if (isHittingRoom(newPosition, "seat6")) {
+      set({room: 'seat-6'})
+    }
+    else if (isHittingRoom(newPosition, "seat7")) {
+      set({room: 'seat-7'})
+    }
+    else if (isHittingRoom(newPosition, "seat8")) {
+      set({room: 'seat-8'})
+    }
+    else if (isHittingRoom(newPosition, "seat9")) {
+      set({room: 'seat-9'})
+    }
+    else if (isHittingRoom(newPosition, "seat10")) {
+      set({room: 'seat-10'})
+    }
+    else if (isHittingRoom(newPosition, "seat11")) {
+      set({room: 'seat-11'})
+    }
+    else if (isHittingRoom(newPosition, "seat12")) {
+      set({room: 'seat-12'})
+    }
+    else if (isHittingRoom(newPosition, "seat13")) {
+      set({room: 'seat-13'})
+    }
+    else if (isHittingRoom(newPosition, "seat14")) {
+      set({room: 'seat-14'})
+    }
+    else if (isHittingRoom(newPosition, "seat15")) {
+      set({room: 'seat-15'})
+    }
+    else if (isHittingRoom(newPosition, "seat16")) {
+      set({room: 'seat-16'})
+    }
+    else if (isHittingRoom(newPosition, "seat17")) {
+      set({room: 'seat-17'})
+    }
+    else if (isHittingRoom(newPosition, "seat18")) {
+      set({room: 'seat-18'})
+    }
+    else if (isHittingRoom(newPosition, "seat19")) {
+      set({room: 'seat-19'})
+    }
+    else if (isHittingRoom(newPosition, "seat20")) {
+      set({room: 'seat-20'})
+    }
+    else if (isHittingRoom(newPosition, "seat21")) {
+      set({room: 'seat-21'})
+    }
+    else if (isHittingRoom(newPosition, "seat22")) {
+      set({room: 'seat-22'})
+    }
+    else if (isHittingRoom(newPosition, "seat23")) {
+      set({room: 'seat-23'})
+    }
+    else if (isHittingRoom(newPosition, "seat24")) {
+      set({room: 'seat-24'})
+    }
+    else if (isHittingRoom(newPosition, "seat25")) {
+      set({room: 'seat-25'})
+    }
+    else {
       set({room: 'default'})
     }
   }
