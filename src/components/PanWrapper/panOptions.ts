3dd4d4a56529403d1788cc0ce050d41ac0ba733e
Martin Wisniowski <mw@node3000.com>
2021-05-25 16:57:00 +0200

    Messing with panWrapper init-values

--- src/components/PanWrapper/panOptions.ts
+++ src/components/PanWrapper/panOptions.ts
@@ -1,22 +1,34 @@
 import { PropsList } from "react-zoom-pan-pinch/dist/store/interfaces/propsInterface"
 
 export const panOptions = {
-  room: { size: { x: 12000, y: 12000 } },
+  room: { size: { x: 6000, y: 6000 } },
     get user() {
       return {
-        // center the room
-        initialPosition: { x: this.room.size.x / 2 - (Math.random() * 200 - 100), y: this.room.size.y / 2 - (Math.random() * 200 - 100) },
+        // center the room at kasse
+        initialPosition: { x: 600 - (Math.random() * 200 - 100), y: 3400 - (Math.random() * 200 - 100) },
         size: { x: 200, y: 200 },
       }
     },
 }
 
+// spawn point eingang: 600 x 3400
+// spawn point foyer: tba
+// spawn point bühne main: tbs
+
 export const transformWrapperOptions: PropsList = {
   wheel: { step: 50 },
-  scale: 1.0,
+  // scale: 1.0,
+  // scale: 0.5,
+  scale: 0.3,
   // center the window, considering the size of the user view
-  defaultPositionX: -panOptions.user.initialPosition.x +(window.innerWidth-panOptions.user.size.x)/2,
-  defaultPositionY: -panOptions.user.initialPosition.y+(window.innerHeight-panOptions.user.size.y)/2,
+  // defaultPositionX: -panOptions.user.initialPosition.x +(window.innerWidth-panOptions.user.size.x)/2,
+  // defaultPositionY: -panOptions.user.initialPosition.y+(window.innerHeight-panOptions.user.size.y)/2,
+  // Notice: this positions are absolute values, scale is not taken into account!!
+  defaultPositionX: -600 * 0.3 + 500,
+  defaultPositionY: -3700 * 0.3 + 500,
+  // Notice: this positions are absolute values, scale is not taken into account!! So we need to make calculations accordingly
+  // defaultPositionX: -(panOptions.user.initialPosition.x * 0.3) + ((window.innerWidth-panOptions.user.size.x)/2) * 1.7,
+  // defaultPositionY: -(panOptions.user.initialPosition.y * 0.3) + ((window.innerWidth-panOptions.user.size.y)/2) * 1.7,
   positionX: 0,
   positionY: 0,
   options: {
