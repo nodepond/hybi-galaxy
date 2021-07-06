df7fc9ddd208f0e17b8c067e7080569bb018ae41
Martin Wisniowski <mw@node3000.com>
2021-07-06 15:26:08 +0200

    Remove old comments

--- src/components/User/Users.tsx
+++ src/components/User/Users.tsx
@@ -2,15 +2,13 @@ import * as React from 'react';
 import { useConferenceStore } from './../../store/ConferenceStore';
 import { User } from "./User"
 
-//TODO check if incorporation of user Object causes rerendering problems
 export const Users = () => {
-
   const {users} = useConferenceStore()
   return (
     <>
     {Object.entries(users).map(user => {
       return(
-          <User key={user[0]} user={user[1]} id={user[0]}/>
+        <User key={user[0]} user={user[1]} id={user[0]} />
       )
     })}
     </>
