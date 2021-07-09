273cc0e82a462e4f5093e547e5f7fcdd32ee56ed
Martin Wisniowski <mw@node3000.com>
2021-07-09 21:25:17 +0200

    Hide special users in foyer

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { useConferenceStore } from '../../store/ConferenceStore';
 import { ReloadHint } from '../ReloadHint/ReloadHint';
 import { AudioTrack } from './AudioTrack';
@@ -7,24 +7,26 @@ import { VideoTrack } from './VideoTrack';
 import { NameTag } from '../NameTag/NameTag';
 
 export const User = ({id, user}) => {
-
   const myPos = useConferenceStore(useCallback(store => store.users[id]['pos'], [id]))
   const myVolume = useConferenceStore(useCallback(store => store.users[id]['volume'], [id]))
   const myRoom = useConferenceStore(useCallback(store => store.users[id]['room'], [id]))
   const isMute = useConferenceStore(useCallback(store => store.users[id]['mute'],[id]))
   const calculateVolume = useConferenceStore(useCallback(store => store.calculateVolume, []))
+  
+  const opacity = useRef();
+
   useEffect(() => {
     calculateVolume(id)
+    
+    // workaround to hide speaker und beamer user in foyer
+    const isOrigin = (myPos.x === 0 && myPos.y === 0)
+    isOrigin ? opacity.current = 0 : opacity.current = 100
+    //console.log('User.js***', myPos.x, myPos.y, opacity.current)
+
   },[id, calculateVolume, myPos])
 
-  let opacity = 100
-  // don't draw special users
-  if (id === 'speaker' || id === 'beamer') {
-    opacity = 0
-  }
-
   return(
-    <div style={{position:'absolute', left:`${myPos.x}px`, top:`${myPos.y}px`, opacity: `${opacity}`}} className="userContainer" >
+    <div style={{ position:'absolute', left:`${myPos.x}px`, top:`${myPos.y}px`, opacity: `${opacity.current}` }} className="userContainer" >
       <VideoTrack id={id} />
       <ReloadHint />
       <AudioTrack id={id} volume={myVolume} />
