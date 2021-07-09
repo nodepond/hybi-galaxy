2c66d9ee2310878dd2ef2a2944f02a7d833ff164
Martin Wisniowski <mw@node3000.com>
2021-07-09 23:08:52 +0200

    Beamer Users View

--- src/components/User/BeamerUsers.tsx
+++ src/components/User/BeamerUsers.tsx
@@ -5,10 +5,18 @@ import { BeamerUser } from './BeamerUser'
 
 // https://1linelayouts.glitch.me
 // https://codepen.io/una/pen/oNbvNQv
-const BeamerUsersContainer = styled.div`
+const BeamerUsersBigGridContainer = styled.div`
+  background-color: black;
   display: grid;
   height: 100vh;
-  grid-gap: 1rem;
+  grid-gap: 0rem;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+`
+const BeamerUsersContainer = styled.div`
+  background-color: black;
+  display: grid;
+  height: 50vh;
+  grid-gap: 0rem;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 `
 
@@ -17,32 +25,49 @@ export const BeamerUsers = () => {
   const usersArray = Object.entries(users)
   const allowedRooms = ['speaker', 'stage', 'seat-1', 'seat-2', 'seat-3', 'seat-4', 'seat-5', 'seat-6', 'seat-7', 'seat-8', 'seat-9', 'seat-10', 'seat-11', 'seat-12', 'seat-13', 'seat-14', 'seat-15', 'seat-16', 'seat-17', 'seat-18', 'seat-19', 'seat-20', 'seat-21', 'seat-22', 'seat-23', 'seat-24']
   const beamerUsers = usersArray.filter(user => {
-    // console.log('user[1].room', user)
-    // console.log('user[1].room xx', user[1].pos.x === 0)
-    // console.log('user[1].room yy', user[1].pos.y === 0)
-    // console.log('user[1].room zz', user[1].pos.x === 0 && user[1].pos.y === 0)
-    
-    // const isOrigin = (user[1].pos.x === 0 && user[1].pos.y === 0)
-    // console.log('isOrigin', isOrigin)
-
-    // return !isOrigin
-    // console.log('user***helo', user, user[1].room.toString(), allowedRooms.includes(user[1].room.toString()))
-
     return (user[1].video && allowedRooms.includes(user[1].room.toString()))
   })
 
+  // let refreshToggle = true
+
+  // function refresh () {
+  //   console.log('refresh... 1')
+  //   beamerUsers.map(user => user[1]?.video?.play())
+  //   refreshToggle = false
+  //   setTimeout(() => {
+  //     console.log('refresh... 2')
+  //     refreshToggle = true
+  //   }, 1000)
+  // }
+
   return (
-    beamerUsers.length === 0
-      ? <div>Aktuell keine Besucher*innen</div>
-      : <BeamerUsersContainer>
-        {beamerUsers.map(user => {
-          return (
-            <>
-              <BeamerUser key={user[0]} user={user[1]} id={user[0]} />
-            </>
-          )
-        })
-        }
-      </BeamerUsersContainer>
+    <>
+      {beamerUsers.length === 0 &&
+        <div>Aktuell keine Besucher*innen</div>}
+    
+      {beamerUsers.length <= 4 &&
+        <BeamerUsersBigGridContainer>
+          {beamerUsers.map(user => {
+            return (
+              <>
+                <BeamerUser key={user[0]} user={user[1]} id={user[0]} />
+              </>
+            )
+          })
+          }
+        </BeamerUsersBigGridContainer>}
+
+      {beamerUsers.length > 4 &&
+        <BeamerUsersContainer>
+          {beamerUsers.map(user => {
+            return (
+              <>
+                <BeamerUser key={user[0]} user={user[1]} id={user[0]} />
+              </>
+            )
+          })
+          }
+        </BeamerUsersContainer>}
+    </>
   )
 }
