bf7c07964ecd44812208a1462603b2c12f698079
Martin Wisniowski <mw@node3000.com>
2021-07-09 20:15:56 +0200

    linting

--- src/components/User/BeamerUser.js
+++ src/components/User/BeamerUser.js
@@ -1,11 +1,11 @@
-import React from 'react';
-// import { useConferenceStore } from '../../store/ConferenceStore';
-import { BeamerVideoTrack } from './BeamerVideoTrack';
+import React from 'react'
+// import { useConferenceStore } from '../../store/ConferenceStore'
+import { BeamerVideoTrack } from './BeamerVideoTrack'
 
 export const BeamerUser = ({id, user}) => {
   // const myRoom = useConferenceStore(useCallback(store => store.users[id]['room'], [id]))
 
-  return(
+  return (
     <div className="userContainer" >
       <BeamerVideoTrack id={id} />
     </div>
