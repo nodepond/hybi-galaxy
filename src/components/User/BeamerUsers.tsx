602f0e2ced10441ce9e8142c25940c2dd5d85769
Martin Wisniowski <mw@node3000.com>
2021-07-09 20:16:07 +0200

    Fixing beamer users logic

--- src/components/User/BeamerUsers.tsx
+++ src/components/User/BeamerUsers.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import styled from "styled-components";
-import { useConferenceStore } from '../../store/ConferenceStore';
-import { BeamerUser } from "./BeamerUser"
+import * as React from 'react'
+import styled from 'styled-components'
+import { useConferenceStore } from '../../store/ConferenceStore'
+import { BeamerUser } from './BeamerUser'
 
 // https://1linelayouts.glitch.me
 // https://codepen.io/una/pen/oNbvNQv
@@ -13,21 +13,36 @@ const BeamerUsersContainer = styled.div`
 `
 
 export const BeamerUsers = () => {
-  const {users} = useConferenceStore()
+  const { users } = useConferenceStore()
   const usersArray = Object.entries(users)
-  // const speakers = usersArray.filter(user => {
-  //   return user[1].room === 'speaker'
-  // })
+  const allowedRooms = ['speaker', 'stage', 'seat-1', 'seat-2', 'seat-3', 'seat-4', 'seat-5', 'seat-6', 'seat-7', 'seat-8', 'seat-9', 'seat-10', 'seat-11', 'seat-12', 'seat-13', 'seat-14', 'seat-15', 'seat-16', 'seat-17', 'seat-18', 'seat-19', 'seat-20', 'seat-21', 'seat-22', 'seat-23', 'seat-24']
+  const beamerUsers = usersArray.filter(user => {
+    // console.log('user[1].room', user)
+    // console.log('user[1].room xx', user[1].pos.x === 0)
+    // console.log('user[1].room yy', user[1].pos.y === 0)
+    // console.log('user[1].room zz', user[1].pos.x === 0 && user[1].pos.y === 0)
+    
+    // const isOrigin = (user[1].pos.x === 0 && user[1].pos.y === 0)
+    // console.log('isOrigin', isOrigin)
+
+    // return !isOrigin
+    // console.log('user***helo', user, user[1].room.toString(), allowedRooms.includes(user[1].room.toString()))
+
+    return (user[1].video && allowedRooms.includes(user[1].room.toString()))
+  })
 
   return (
-    usersArray.length === 0 ? 
-    <div>Keiner Besucher*innen im Foyer</div> :
-    <BeamerUsersContainer>
-      {usersArray.map(user => {
-        return(
-          <BeamerUser key={user[0]} user={user[1]} id={user[0]}/>
-        )}
-      )}
-    </BeamerUsersContainer>
+    beamerUsers.length === 0
+      ? <div>Aktuell keine Besucher*innen</div>
+      : <BeamerUsersContainer>
+        {beamerUsers.map(user => {
+          return (
+            <>
+              <BeamerUser key={user[0]} user={user[1]} id={user[0]} />
+            </>
+          )
+        })
+        }
+      </BeamerUsersContainer>
   )
 }
