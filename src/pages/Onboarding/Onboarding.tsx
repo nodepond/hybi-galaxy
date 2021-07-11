39531946e348c31302ca186d268e040dd19b464f
Martin Wisniowski <mw@node3000.com>
2021-07-11 21:51:12 +0200

    Lint

--- src/pages/Onboarding/Onboarding.tsx
+++ src/pages/Onboarding/Onboarding.tsx
@@ -9,21 +9,21 @@ import { SubHeadline } from '../../components/common/SubHeadline'
 import { Info } from '../../components/common/Info/Info'
 
 const BigHeadContainer = styled.div`
-	display: flex;
-	flex-direction: row;
-	justify-content: center;
-	align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 `
 
 const CenterContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
 `
 
 const FormContainer = styled.div`
-	margin-top: 40px;
+  margin-top: 40px;
 `
 
 export const Onboarding = () => {
