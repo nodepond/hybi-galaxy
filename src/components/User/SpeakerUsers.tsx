a28d0be214e2f276366dfae60940f088f492afb5
Martin Wisniowski <mw@node3000.com>
2021-07-11 20:44:38 +0200

    Speaker users grid view

--- src/components/User/SpeakerUsers.tsx
+++ src/components/User/SpeakerUsers.tsx
@@ -17,10 +17,6 @@ const LogoContainer = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-
-  & video {
-    height: 100%;
-  }
 `
 const LogoFade = styled.div`
   display: flex;
@@ -33,20 +29,35 @@ const LogoFade = styled.div`
   animation-duration: 24s;
   animation-iteration-count: 1;
   animation-fill-mode: forwards;
+`
+
+// https://1linelayouts.glitch.me
+// https://codepen.io/una/pen/oNbvNQv
+const SpeakerUsersOneRowContainer = styled.div`
+  background-color: black;
+  display: grid;
+  height: 100vh;
+  grid-gap: 0rem;
+  grid-template-columns: repeat(auto-fit, minmax(642px, 1fr));
+  grid-template-rows: 100%;
+  overflow: hidden;
 
   & video {
     height: 100%;
   }
 `
-
-// https://1linelayouts.glitch.me
-// https://codepen.io/una/pen/oNbvNQv
 const SpeakerUsersBigGridContainer = styled.div`
   background-color: black;
   display: grid;
   height: 100vh;
   grid-gap: 0rem;
-  grid-template-columns: repeat(auto-fit, minmax(610px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(642px, 1fr));
+  grid-template-rows: 50% 50%;
+  overflow: hidden;
+
+  & video {
+    height: 100%;
+  }
 `
 const SpeakerUsersContainer = styled.div`
   background-color: black;
@@ -54,6 +65,11 @@ const SpeakerUsersContainer = styled.div`
   height: 50vh;
   grid-gap: 0rem;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  overflow: hidden;
+
+  & video {
+    max-height: 100vh;
+  }
 `
 
 export const SpeakerUsers = () => {
@@ -70,8 +86,20 @@ export const SpeakerUsers = () => {
             <LogoSvg />
           </LogoFade>
         </LogoContainer>}
+
+      {speakerUsers.length !== 0 && speakerUsers.length <= 2 &&
+        <SpeakerUsersOneRowContainer>
+          {speakerUsers.map(user => {
+            return (
+              <>
+                <SpeakerUser key={user[0]} user={user[1]} id={user[0]} />
+              </>
+            )
+          })
+          }
+        </SpeakerUsersOneRowContainer>}
     
-      {speakerUsers.length <= 4 &&
+        {speakerUsers.length > 2 && speakerUsers.length <= 4 &&
         <SpeakerUsersBigGridContainer>
           {speakerUsers.map(user => {
             return (
