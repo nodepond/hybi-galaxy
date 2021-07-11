e68e7f48762805e4ed0729c803af723bb81d48e7
Martin Wisniowski <mw@node3000.com>
2021-07-11 21:55:37 +0200

    Mobile-Selector Session-View

--- src/pages/Session/Session.tsx
+++ src/pages/Session/Session.tsx
@@ -16,16 +16,19 @@ import { PanWrapper } from '../../components/PanWrapper/PanWrapper'
 import { Room } from '../../components/Room/Room'
 import { Users } from '../../components/User/Users'
 import { LocalStoreLogic } from '../../store/LocalStoreLogic'
+import { useMediaQuery } from 'react-responsive'
 
 export const Session = () => {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: '(min-device-width: 1224px)'
+  })
+
   return (
     <React.Fragment>
       <ErrorHandler />
-      {/* <Info>
-        Welcome to our Prototype
-        <br />
-        Please use <b>Chrome</b> for now for a stable Experience
-      </Info> */}
+      {!isDesktopOrLaptop && <Info>
+        Das Foyer funktionert aktuell nicht auf Mobiltelefon oder Tablet. Bitte benutzen Sie einen Browser auf dem Laptop oder PC.
+      </Info>}
       <Header>Hybrid Stage by BoatPeopleProject e.V.</Header>
       <JitsiConnection />
       <LocalStoreLogic />
